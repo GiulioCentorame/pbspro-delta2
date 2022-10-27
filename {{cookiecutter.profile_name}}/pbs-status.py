aaf92a5dd8f97776557c4ebc5bdb80fc22a47115
Giulio Centorame <46871438+GiulioCentorame@users.noreply.github.com>
2022-10-27 19:05:33 +1000

    Match delta2's job naming convention

--- {{cookiecutter.profile_name}}/pbs-status.py
+++ {{cookiecutter.profile_name}}/pbs-status.py
@@ -7,7 +7,7 @@ import xml.etree.cElementTree as ET
 jobid = sys.argv[1]
 
 try:
-    res = subprocess.run("qstat -f -x {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    res = subprocess.run("qstat -f -x {}.delta2".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
     xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
     job_state = xmldoc.findall('.//job_state')[0].text
